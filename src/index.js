16fe04d37633640767c80208c1dbeaecb2045a60
sumansaksh <suman_fw12_142@masai.school>
2021-12-15 10:01:07 +0530

    backend authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 let ejs=require("ejs")
 
+const { register, login } = require("./controllers/auth.controller");
+
+
+
 const productController = require("./controllers/product.controller");
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/register", register);
+app.post("/login", login);
+
 
 app.use(express.urlencoded({extended: false }))
 app.use(express.static("public"))
