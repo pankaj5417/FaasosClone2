445956097d9279530865fc3375981998294110a4
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-02-12 22:54:14 +0530

    whatever

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require("./index");
 const connect = require("./configs/db");
+const port=process.env.PORT || 8000
 
-app.listen(process.env.PORT||8080, async function () {
+app.listen(port, async function () {
   await connect();
   console.log("listening on port 8080");
 });
