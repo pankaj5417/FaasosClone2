6385ddc1306f710213e035e4116e74dd17d74cd9
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-02-12 15:47:09 +0530

    comit

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(process.env.MongoURL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
+    //useNewUrlParser:true,
+    //useUnifiedTopology:true,
+    //useCreateIndex:true,
     
   });
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./index");
 const connect = require("./configs/db");
 
-app.listen(process.env.PORT||2345, async function () {
+app.listen(process.env.PORT||8080, async function () {
   await connect();
-  console.log("listening on port 2345");
+  console.log("listening on port 8080");
 });
